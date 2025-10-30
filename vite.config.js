9eac08a97059fe813d759918b018b0caef0d8220
justinrayver <justinrayver.14@gmail.com>
2025-10-30 14:56:47 +0800

    Fix vite config and add vercel setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // add this for static hosting
-});
+  base: './', // <-- this line helps Vercel find your assets
+})
