f4ae924a147b746f06dc5de87c79e044f995be44
justinrayver <justinrayver.14@gmail.com>
2025-10-30 15:13:55 +0800

    Removed preloader and updated styles

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,12 +1,5 @@
 import React from "react";
 
 export default function Preloader() {
-  return (
-    <div id="preloader">
-      <div className="preloader-inner">
-        <div className="loader-orb"></div>
-        <div className="preloader-name">Loading...</div>
-      </div>
-    </div>
-  );
+  return null;
 }
